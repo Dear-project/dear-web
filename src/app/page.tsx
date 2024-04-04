3f89b418d1572e7c4adb2441080f560b00503437
greenfrog616 <greenfrog0616@gmail.com>
2024-04-04 23:23:27 +0900

    main category,professor making

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Image from "next/image";
-
+import Main from "src/Components/Home/Category";
+import Pro from "src/Components/Home/Professor";
 export default function Home() {
   return (
-   <>
-   </>
+    <>
+      <Main />
+    </>
   );
 }
