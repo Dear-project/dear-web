10a6f0436455a442dc046c41fe898189d63dca2a
greenfrog616 <greenfrog0616@gmail.com>
2024-04-04 23:27:38 +0900

    feat:: category,professor #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-"use client";
 import Image from "next/image";
-import Main from "src/Components/Home/Category";
-import Pro from "src/Components/Home/Professor";
+
 export default function Home() {
   return (
-    <>
-      <Main />
-    </>
+   <>
+   </>
   );
 }
