7fcfc03647faac0a50ec425c1ad4d1f764ff8e56
jeonminchan <132974029+wjzlskxk@users.noreply.github.com>
2024-04-12 12:21:08 +0900

    design :: global.css -> global.ts로 변경 #1

---
+++ src/style/global.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyle = createGlobalStyle`
+${reset}
+@import url("https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap");
+*{
+    margin: 0;
+    padding: 0;
+    border: 0;
+}
+`;
+
+export default globalStyle;
