2e2ccbca97f39ed6d725160e4115450e16e890bb
greenfrog616 <greenfrog0616@gmail.com>
2024-05-12 21:33:23 +0900

    feature #2 banner

--- src/Components/Home/Header/style.ts
+++ src/Components/Home/Header/style.ts
@@ -1,21 +1,26 @@
 import styled from "styled-components";
-import Image31 from "../../../asset/Ellipse 31.png";
+import Image from "next/image";
+import Image31 from "public/Ellipse 31.png";
+
 export const Main = styled.div`
   position: relative;
   display: flex;
   width: 100%;
   flex-direction: column;
-
   align-items: center;
 `;
+
 export const Banner = styled.div`
   border-radius: 17px;
   background: var(--New, #0e2764);
   width: 65vw;
   height: 20vh;
+  margin-left: 15%;
+  margin-top: 3%;
   display: flex;
   justify-content: space-between;
 `;
+
 export const Textbunddle = styled.div`
   margin-left: 8%;
   margin-top: 1%;
@@ -23,6 +28,7 @@ export const Textbunddle = styled.div`
   justify-content: flex-start;
   flex-direction: column;
 `;
+
 export const Text = styled.div`
   color: #fff;
   font-family: Pretendard;
@@ -31,6 +37,7 @@ export const Text = styled.div`
   font-weight: 500;
   line-height: normal;
 `;
+
 export const Text1 = styled.div`
   color: #fff;
   font-family: Assistant;
@@ -39,10 +46,38 @@ export const Text1 = styled.div`
   font-weight: 800;
   line-height: normal;
 `;
-export const Button = styled.button`
+export const ButtonWrapper = styled.div`
+  position: relative;
   width: 100px;
   height: 70px;
-  background-image: url(${Image31});
-  background-size: cover;
-  background-position: center;
+  margin-right: 8%;
+  margin-top: 8%;
+`;
+
+export const ButtonImage = styled(Image).attrs({
+  src: Image31,
+  alt: "Icon",
+  width: 100,
+  height: 70,
+})``;
+
+export const ButtonText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #000;
+  font-family: Assistant;
+  font-size: 0.875rem;
+  font-weight: 800;
+`;
+
+export const Button = styled.button`
+  all: unset;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
 `;
