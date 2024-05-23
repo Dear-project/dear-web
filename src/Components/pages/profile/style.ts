9a370ea621661c578d76d3700825614bd9e6810f
greenfrog616 <greenfrog0616@gmail.com>
2024-05-23 15:13:44 +0900

    fix:: css #9

--- src/Components/pages/profile/style.ts
+++ src/Components/pages/profile/style.ts
@@ -1,17 +1,15 @@
 import styled from "styled-components";
 export const ABox = styled.div`
-  position: relative;
+  height: 100vh; // 화면 높이의 100%
+  width: 100vw;
   display: flex;
-  width: 100%;
-  height: 100vh; // 화면 전체 높이 차지
-  flex-direction: column;
-  align-items: center; // 수직 중앙 정렬
-  justify-content: center; // 수평 중앙 정렬
+  align-items: center;
+  justify-content: center;
 `;
 
 export const BBox = styled.div`
-  width: 65vw;
-  height: 80vh;
+  width: 80%;
+  height: 100%;
   background-color: #fff;
   border-radius: 1.0625rem;
 `;
