7b5fc6608bc42d9961919fd7b23707ff45260a7b
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-17 17:58:35 +0900

    chore :: 충돌해결

--- src/style/global.ts
+++ src/style/global.ts
@@ -4,13 +4,20 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-${reset}
-@import url("https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap");
-*{
-    margin: 0;
-    padding: 0;
-    border: 0;
-}
+@font-face {
+    font-family: "Pretendard-Medium";
+    src: url("../assets/font/Pretendard/Pretendard-Medium.otf");
+  }
+      ${reset}
+    * {
+      margin: 0;
+      padding: 0;
+      border: none;
+      color:black;
+      font-family: "Pretendard-Medium" !important;
+    }
+    a {
+      text-decoration: none;
+    }
 `;
-
 export default GlobalStyle;
