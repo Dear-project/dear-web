7d28b1806c0ec48481cc8b7211a4090634a887dc
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-17 10:54:58 +0900

    fix :: 충돌

--- src/Components/Auth/login/index.tsx
+++ src/Components/Auth/login/index.tsx
@@ -3,10 +3,10 @@
 import React from "react";
 import * as S from "./style";
 import useLogin from "src/hooks/Auth/login/useLogin";
-import TextField from "src/components/common/TextField";
+import TextField from "src/Components/common/TextField";
 import { useRouter } from "next/navigation";
 import { useRecoilState } from "recoil";
-import { ErrorStateAtom } from "src/stores/common/common.store";
+import { ErrorStateAtom } from "src/store/common/common.store";
 
 const Login = () => {
   const { ...hooks } = useLogin();
@@ -22,8 +22,8 @@ const Login = () => {
           value={hooks.LoginData.userId}
           functions="userId"
           onchange={hooks.handleLoginChange}
+          labelStyle={{ top: "37%" }}
           type="email"
-          labelStyle={{ top: "47%" }}
         >
           이메일
         </TextField>
@@ -34,7 +34,7 @@ const Login = () => {
           value={hooks.LoginData.password}
           functions={hooks.handleConfirmButton}
           onchange={hooks.handleLoginChange}
-          labelStyle={{ top: "56%" }}
+          labelStyle={{ top: "49%" }}
         >
           비밀번호
         </TextField>
