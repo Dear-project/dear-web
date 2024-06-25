a126bc90bc280142e38f53e522aae5305da70461
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-25 22:05:14 +0900

    fix :: sidebar

--- src/hooks/sidebar/useSidebar.ts
+++ src/hooks/sidebar/useSidebar.ts
@@ -6,55 +6,46 @@ import { useSidebarStore } from "src/store/common/sidebar/sidebar.store";
 import axios from "axios";
 
 const useSidebar = () => {
-  const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
-  const selectedItem = useSidebarStore((state) => state.selectedItem);
-  const setSelectedItem = useSidebarStore((state) => state.setSelectedItem);
-  const pathname = usePathname();
-  const router = useRouter();
+  // const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
+  // const selectedItem = useSidebarStore((state) => state.selectedItem);
+  // const setSelectedItem = useSidebarStore((state) => state.setSelectedItem);
+  // const pathname = usePathname();
+  // const router = useRouter();
 
-  const handleLogoclick = (item: any) => {
-    setSelectedItem(""); // 로고 클릭 시 선택된 버튼 초기화
-    setSelectedItem(item);
-    if (item === "main") {
-      router.push("/main");
-    } else {
-      router.push(`/path/${item}`);
-    }
-  };
 
-  const fetchUserProfile = async () => {
-    try {
-      const response = await axios.get(`${config.serverUrl}/profile`);
-      console.log("User profile fetched successfully:", response.data); // 응답 데이터 로그
-      setUserProfile(response.data);
-    } catch (error) {
-      console.error("Error fetching user profile:", error);
-    }
-  };
 
-  useEffect(() => {
-    fetchUserProfile();
-  }, []);
+  // const fetchUserProfile = async () => {
+  //   try {
+  //     const response = await axios.get(`${config.serverUrl}/profile`);
+  //     console.log("User profile fetched successfully:", response.data); // 응답 데이터 로그
+  //     setUserProfile(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching user profile:", error);
+  //   }
+  // };
 
-  // 페이지 로드 시 세션 스토리지에서 선택된 버튼 정보를 읽어와 selectedItem 상태 업데이트
-  useEffect(() => {
-    const storedItem = sessionStorage.getItem("selectedItem");
-    if (storedItem) {
-      setSelectedItem(storedItem);
-    }
-  }, [setSelectedItem]);
+  // useEffect(() => {
+  //   fetchUserProfile();
+  // }, []);
 
-  useEffect(() => {
-    console.log("User profile state updated:", userProfile); // userProfile 상태 변경 로그
-  }, [userProfile]);
+  // // 페이지 로드 시 세션 스토리지에서 선택된 버튼 정보를 읽어와 selectedItem 상태 업데이트
+  // useEffect(() => {
+  //   const storedItem = sessionStorage.getItem("selectedItem");
+  //   if (storedItem) {
+  //     setSelectedItem(storedItem);
+  //   }
+  // }, [setSelectedItem]);
+
+  // useEffect(() => {
+  //   console.log("User profile state updated:", userProfile); // userProfile 상태 변경 로그
+  // }, [userProfile]);
 
   return {
-    fetchUserProfile,
-    selectedItem,
-    handleLogoclick,
-    userProfile,
-    pathname,
-    router,
+    // fetchUserProfile,
+    // selectedItem,
+    // userProfile,
+    // pathname,
+    // router,
   };
 };
 
