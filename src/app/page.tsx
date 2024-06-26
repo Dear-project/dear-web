df3b3613e2ef13caef8d6ef982d57d5298e637bf
greenfrog616 <greenfrog0616@gmail.com>
2024-06-26 12:21:28 +0900

    feat:: main page view  #25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 import Image from "next/image";
-import Mainpage from "@/components/home/category/index";
+import Mainpage from "../app/main/page";
 import Sidebar from "@/components/common/sidebar";
 
 const Home = () => {
