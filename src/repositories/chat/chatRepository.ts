6e8c60acee01fe65f951f1bab83683900089ef7f
해준 <127281057+ftery0@users.noreply.github.com>
2024-10-13 13:15:59 +0900

    remove :: 파일정리

--- src/repositories/chat/chatRepository.ts
+++ src/repositories/chat/chatRepository.ts
@@ -1,7 +1,8 @@
-import { ChatGetResponse } from "../../types/chat/chat.type";
+import { ChatGetResponse,ChatSearchProps } from "../../types/chat/chat.type";
+
 
 export interface ChatRepository {
     getChat(userId: number): Promise<ChatGetResponse>;
-  
+    chatSearch({userId, word}:ChatSearchProps):Promise<ChatGetResponse>
   }
 
--- src/repositories/chat/chatRepositoryImpl.ts
+++ src/repositories/chat/chatRepositoryImpl.ts
@@ -1,5 +1,5 @@
 import { ChatRepository } from "./chatRepository";
-import { ChatGetResponse } from "../../types/chat/chat.type";
+import { ChatGetResponse,ChatSearchProps } from "../../types/chat/chat.type";
 import dearAxios from "@/libs/axios/customAxios";
 
  class ChatRepositoryImpl implements ChatRepository {
@@ -7,6 +7,12 @@ import dearAxios from "@/libs/axios/customAxios";
         const {data} = await dearAxios.get('chat/get',{params:userId});
         return data
     }
+    public async chatSearch({userId, word}:ChatSearchProps):Promise<ChatGetResponse>{
+        const {data} = await dearAxios.get(`/chat/search?userId=${userId}&word=${word}`);
+        return data
+    }
+
+
    
  }
  export default new ChatRepositoryImpl
