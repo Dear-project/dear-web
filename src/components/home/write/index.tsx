2c7e267818ecd29bcc2caf7b168c4dada4a9f918
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-10-17 22:05:24 +0900

    feat :: onWrite

--- src/components/home/write/index.tsx
+++ src/components/home/write/index.tsx
@@ -11,43 +11,6 @@ const Write = () => {
   const { ...write } = useWrite();
 
   return (
-    // <S.Write>
-    //   <S.Main>
-    //     <S.TitleIinput
-    //       placeholder="제목"
-    //       name="title"
-    //       type="text"
-    //       onChange={write.handleWriteData}
-    //       value={write.writeData.title}
-    //     />
-    //     <div style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}>
-    //       <PreviewImage previewImage={write.image ? write.image : []} onclick={() => write.setImage([])} />
-    //     </div>
-    //     <S.ContentInput
-    //       placeholder="내용"
-    //       name="content"
-    //       onChange={write.handleWriteData}
-    //       value={write.writeData.content}
-    //     />
-    //   </S.Main>
-    //   <S.ButtonWrap>
-    //     {write.isClicked === true && (
-    //       <div>
-    //         <Image src={ConfirmButton} alt="완료 버튼" onClick={write.onWrite} />
-    //         <Image src={ImageUpload} alt="이미지 업로드 버튼" onClick={write.handleImageClick} />
-    //         <input
-    //           ref={write.ImageRef}
-    //           type="file"
-    //           multiple
-    //           style={{ display: "none" }}
-    //           accept=".jpg, .jpeg, .png"
-    //           onChange={write.handleImageChange}
-    //         />
-    //       </div>
-    //     )}
-    //     <Image className="button" src={write.isClicked ? CloseButton : Dropdown} alt="" onClick={write.onClick} />
-    //   </S.ButtonWrap>
-    // </S.Write>
     <S.Write>
       <S.WriteWrap>
         <S.TitleWrap>
@@ -83,7 +46,7 @@ const Write = () => {
             <h1>이미지 게시</h1>
           </div>
         </S.ImageWrap>
-        <S.Button>게시하기</S.Button>
+        <S.Button onClick={write.onWrite}>게시하기</S.Button>
       </S.ToolsWrap>
       <Image src={WriteGroomy} alt="귀여운놈" className="cute" />
     </S.Write>
