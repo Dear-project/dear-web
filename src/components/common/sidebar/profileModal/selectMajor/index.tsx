d95841904203c2026d4bff2385578b2e8ce97f51
해준 <127281057+ftery0@users.noreply.github.com>
2024-10-20 01:12:03 +0900

    refactor :: profile

---
+++ src/components/common/sidebar/profileModal/selectMajor/index.tsx
@@ -0,0 +1,65 @@
+import React ,{useState}from "react";
+import * as S from "./style";
+import Image from "next/image";
+import Search from "@/asset/search.svg";
+import useSelectModal from "@/hooks/firstLogin/selectModal";
+import { useGetMajorBySubject } from "@/queries/firstLogin/firstLogin.query";
+import { GetMajorListReposne } from "@/types/firstLogin/firstLogin.types";
+import { MAJOR_TYPE_LIST } from "@/constants/majorType/majorType.constants";
+import convertMajorListType from "@/utils/majorList/convertMajorListType";
+
+const SelectMajor = ()=>{
+    const {...select}= useSelectModal();
+    const [selectedMajor, setSelectedMajor] = useState<string | null>(null);
+    const searchMajorBySubjectMutation = useGetMajorBySubject();
+
+    
+    return(
+        <>
+              <S.Header>
+        <S.Title>자신의 학과 선택</S.Title>
+        <S.SearchWrap>
+          <input type="text" placeholder="학과를 입력해주세요" value={select.keyword} onChange={select.handleKeyword} />
+          <Image src={Search} alt="돋보기" width={20} height={20} onClick={select.searchMajorList} />
+        </S.SearchWrap>
+      </S.Header>
+      <S.MajorBoxWrap>
+        <S.MajorWrap>
+          <S.MajorTypeWrap>
+            {MAJOR_TYPE_LIST.map((item, idx) => (
+              <S.MajorTypeList
+                key={idx}
+                $isclicked={select.subject === item ? "true" : "false"}
+                onClick={() => {
+                  const params = { subject: item };
+                  searchMajorBySubjectMutation.mutate(params, {
+                    onSuccess: (data) => {
+                      select.setMajorList(data);
+                    },
+                  });
+                }}
+              >
+                <span>{convertMajorListType.convertMajorToString(item)}</span>
+              </S.MajorTypeList>
+            ))}
+          </S.MajorTypeWrap>
+          <S.MajorListWrap>
+            {select.majorList?.data.map((item, idx) => (
+              <S.Major
+                key={idx}
+                $isSelected={selectedMajor === item.majorSeq} 
+                onClick={() => {
+                  setSelectedMajor(item.majorSeq);
+                  select.handleSubmitParams(item.majorSeq, item.lClass, item.mClass);
+                }}
+              >
+                <span>{item.mClass}</span>
+              </S.Major>
+            ))}
+          </S.MajorListWrap>
+        </S.MajorWrap>
+      </S.MajorBoxWrap>
+        </>
+    )
+}
+export default SelectMajor;
