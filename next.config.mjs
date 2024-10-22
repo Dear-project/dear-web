6a89056eee2fb3451d63d87d7dd8995651283f94
해준 <127281057+ftery0@users.noreply.github.com>
2024-10-23 02:58:38 +0900

    fix :: config error

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ["lift-s3-bucket.s3.ap-northeast-2.amazonaws.com"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
